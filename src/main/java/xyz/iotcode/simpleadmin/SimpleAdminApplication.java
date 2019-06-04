9e23a6ca2f358b6a113462478a954e200c5a8789
xieshuang <1312544013@qq.com>
2019-06-04 17:52:35 +0800

    1

---
+++ src/main/java/xyz/iotcode/simpleadmin/SimpleAdminApplication.java
@@ -0,0 +1,13 @@
+package xyz.iotcode.simpleadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleAdminApplication.class, args);
+    }
+
+}
